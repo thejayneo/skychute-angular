5cf9fc1b48f73ec5d2122a5c5d7893daeb5b20a6
Jackson <cas022032@coderacademy.edu.au>
2021-03-09 13:51:53 +1100

    Pre-workshop commit.

--- src/app/helper.service.ts
+++ src/app/helper.service.ts
@@ -1,13 +1,11 @@
 import { Injectable } from '@angular/core';
 import { ParentAppThreeComponent } from './parent-app-three/parent-app-three.component';
 import { Observable, of } from 'rxjs';
-import { ParentAppOneComponent } from './parent-app-one/parent-app-one.component';
 
 @Injectable({
   providedIn: 'root'
 })
 export class HelperService {
-  static helperInput: string;
   constructor() { }
 
   getInput(): Observable<string> {
--- src/app/parent-app-three/parent-app-three.component.ts
+++ src/app/parent-app-three/parent-app-three.component.ts
@@ -1,21 +1,11 @@
-import { Component, OnInit } from '@angular/core';
-import { Observable } from 'rxjs';
+import { Component } from '@angular/core';
 
 @Component({
   selector: 'app-parent-app-three',
   templateUrl: './parent-app-three.component.html',
   styleUrls: ['./parent-app-three.component.css']
 })
-export class ParentAppThreeComponent implements OnInit {
+export class ParentAppThreeComponent {
   currentValue = '';
-  subscribedInput = new Observable(function subscribe(observed): void {
-    observed.next(ParentAppThreeComponent.currentValue);
-  });
-
-  constructor() {
-  }
-
-  ngOnInit(): void {
-  }
 }
 
